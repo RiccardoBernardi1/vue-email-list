43077deea349e1280882b44d2102e44344d78125
RiccardoBernardi1 <rikyb3004@gmail.com>
2022-11-16 14:28:19 +0100

    Crea ciclo for per fare dieci richieste e inserire le email ricevute nella lista delle email

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ createApp({
         return{
             emails:[]
         }
+    },
+    created(){
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response)=> {
+                console.log(response.data.response);
+                this.emails.push(response.data.response);
+                console.log(this.emails);
+            });   
+        }
     }
 }).mount("#app");
